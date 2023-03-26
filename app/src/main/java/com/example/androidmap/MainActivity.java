522e76895b1ef8aaac088d93de854a6aab9770cc
王龙飞 <wanglongfei@wanglongfeideMacBook-Air.local>
2023-03-26 18:07:55 +0800

    gaode success

--- app/src/main/java/com/example/androidmap/MainActivity.java
+++ app/src/main/java/com/example/androidmap/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
     }
 }
